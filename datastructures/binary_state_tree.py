fa7616f529cbc24010ae5d41fa045a9836696725
timothy.reynolds <timothy.reynolds@dsainc.com>
2021-01-20 14:51:24 -0500

    Binary State Tree completed except for post_order_traversal and the delete function

--- datastructures/binary_state_tree.py
+++ datastructures/binary_state_tree.py
@@ -15,10 +15,11 @@ class BST:
             self.insert(data)
 
     def insert(self, data):
-        #Checks for duplicate
-        if self.data == data:
+        if self.data is None:
+            self.data = data
+        elif self.data == data:
             return False
-        if self.data > data:
+        elif self.data > data:
             if self.left is not None:
                 return self.left.insert(data)
             else:
@@ -30,8 +31,11 @@ class BST:
             else:
                 self.right = BST(data)
                 return True
+
     def find(self, data):
-        if self.data == data:
+        if self.data is None:
+            return False
+        elif self.data == data:
             return data
         elif self.data > data:
             if self.left is None:
@@ -44,5 +48,50 @@ class BST:
             else:
                 return self.right.find(data)
 
+    def get_size(self):
+        if self.left is not None and self.right is not None:
+            return 1 + self.left.get_size() + self.right.get_size()
+        elif self.left is None and self.right is not None:
+            return 1 + self.right.get_size()
+        elif self.left is not None and self.right is None:
+            return 1 + self.left.get_size()
+        elif self.left is None and self.right is None:
+            return 1
 
+    def min_value_node(self, parent):
+        if self.left is None:
+            return [self, parent]
+        else:
+            return self.left.min_value_node(self)
+
+    def delete(self, root, key):
+        if root is None:
+            return root
+        elif root.data < key:
+            root.right = root.delete(root.right, key)
+        elif root.data > key:
+            root.left = root.delete(root.left, key)
+        else:
+            # Case 1, No Child
+            if root.right is None and root.left is None:
+                if root.data == key:
+                    root.data = None
+            # Case 2, One Child
+            elif self.left is None and self.right is not None:
+                root = self.right
+            elif self.left is not None and self.right is None:
+                root = self.left
+            else:
+                next_highest, parent = self.right.min_value_node(root.right)
+                root.data = next_highest.data
+                root.right = self.delete(parent.right, next_highest.data)
+        return root
+
+    def in_order_traversal(self, root):
+        res = []
+        if root:
+            res = self.in_order_traversal(root.left)
+            res.append(root.data)
+            res = res + self.in_order_traversal(root.right)
+        return res
 
--- unittesting/bst_test.py
+++ unittesting/bst_test.py
@@ -22,22 +22,66 @@ class BSTTestCases(unittest.TestCase):
 
     def test_find(self):
         self.tree.insert_list([4, 10, 12, 3])
-
         self.assertEqual(self.tree.find(3), 3)
-        # self.assertEqual(self.tree.find(12), 12)
-        # self.assertEqual(self.tree.find(1231), False)
+        self.assertEqual(self.tree.find(12), 12)
+        self.assertEqual(self.tree.find(1231), False)
 
     def test_delete(self):
-        pass
+        # self.tree.insert_list([10, 12, 3, 0, 4])
+        self.assertEqual(self.tree.find(5), 5)
+        self.tree.delete(self.tree, 5)
+        self.assertEqual(self.tree.find(5), False)
+
+        self.tree.insert(5)
+        self.assertEqual(self.tree.find(5), 5)
+
+
+    def test_in_order_traversal(self):
+        self.tree.insert_list([3, 10, 12, 15, 19])
+        sorted_list = self.tree.in_order_traversal(self.tree)
+        self.assertEqual(sorted_list[0], 3)
+        self.assertEqual(sorted_list[-1], 19)
+
 
     def test_get_size(self):
-        pass
+        # Test no insert but init
+        self.assertEqual(self.tree.get_size(), 1)
+
+        # Test single insert
+        self.tree.insert(3)
+        self.assertEqual(self.tree.get_size(), 2)
+
+        # Test multiple inserts
+        self.tree.insert(15)
+        self.tree.insert(-2)
+        self.tree.insert(0)
+        self.assertEqual(self.tree.get_size(), 5)
+
+        # Test Duplicate
+        self.tree.insert(15)
+        self.assertEqual(self.tree.get_size(), 5)
+
+    def test_min_value_node(self):
+        self.assertEqual(self.tree.min_value_node(self.tree)[0].data, 5)
+
+        self.tree.insert(3)
+        self.assertEqual(self.tree.min_value_node(self.tree)[0].data, 3)
+
+        self.tree.insert(-12)
+        self.tree.insert(-4)
+        self.tree.insert(0)
+        self.tree.insert(101)
+        self.tree.insert(-10)
+        self.assertEqual(self.tree.min_value_node(self.tree)[0].data, -12)
+
+
 
     def test_preorder_traversal(self):
         pass
 
     def test_inorder_traversal(self):
         pass
+
 
 
 if __name__ == '__main__':
